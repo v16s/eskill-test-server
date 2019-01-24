3e40c524e14e8d398bf37533522335f841ecaa33
ShriB11 <banerjeeshirirup10@gmail.com>
2019-01-24 09:15:12 +0530

    fixed

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+
+var book = require('./routes/book')
+var auth = require('./routes/auth')
+
+var app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: 'false' }))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.use('/api/book', book)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+app.use('/api/auth', auth)
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+var mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+mongoose
+  .connect(
+    'mongodb://localhost/mern-secure',
+    { promiseLibrary: require('bluebird') }
+  )
+  .then(() => console.log('connection succesful'))
+  .catch(err => console.error(err))
+
+module.exports = app

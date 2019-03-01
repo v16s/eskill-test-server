980d1b2a049f1183828321121cdd308e1f0b6572
Surya Kiran <skiran13@gmail.com>
2019-03-01 22:35:34 +0530

    Global Schema Added with functionality!

--- server/models/index.js
+++ server/models/index.js
@@ -2,9 +2,11 @@ let User = require('./user')
 let Branch = require('./branch')
 let Report = require('./report')
 let Test = require('./test')
+let Global = require('./global')
 module.exports = {
   Test,
   Branch,
   User,
-  Report
+  Report,
+  Global
 }

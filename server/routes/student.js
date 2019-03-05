190e6ac556f36d8cea32dbbb698c117c10444dda
Surya Kiran <skiran13@gmail.com>
2019-03-05 13:56:15 +0530

    Student routes

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,6 +1,30 @@
 let router = require('express').Router()
+let { Report, Branch, Test, User } = require('../models')
 
 router.get('/', (req, res) => {
   res.sendStatus(200)
+})
+router.get('/time', (req, res) => {
+  Report.findOne({ username: req.user.username }, (err, report) => {
+    if (err) {
+      res.json({ success: false, err })
+    } else {
+      res.json({ success: true, time: report.time })
+    }
+  })
+})
+router.post('/updatetime', (req, res) => {
+  Report.findOne({ username: req.user.username }, (err, report) => {
+    if (report.time > req.body.time) {
+      report.time = req.body.time
+      report.save(function (err, rep) {
+        if (err) {
+          res.json({ success: false, err })
+        } else {
+          res.json({ success: true, rep })
+        }
+      })
+    } else res.json({ success: false, err: ' Time is invalid' })
+  })
 })
 module.exports = router
